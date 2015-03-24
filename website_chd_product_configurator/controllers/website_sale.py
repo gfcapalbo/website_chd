2a8645428d2e7f43c25b3ada91ba6bc067ad09b0
gfcapalbo <giovanni@therp.nl>
2015-03-24 17:22:15 +0100

    [ADD] working alternative sender address with database updated correctly

--- website_chd_product_configurator/controllers/website_sale.py
+++ website_chd_product_configurator/controllers/website_sale.py
@@ -4,6 +4,7 @@ from openerp import http
 from openerp.http import request
 from openerp.osv import orm
 import openerp.addons.website_sale.controllers.main as websale
+from telnetlib import theNULL
 
 
 
@@ -16,5 +17,26 @@ class website_sale_ext(websale.website_sale):
                 configurator = pc.Chd_website()
                 return pc.Chd_website.start(configurator,product.id)
         return super(website_sale_ext, self).product(product)
+
+
+
+
+    def checkout_values(self, data=None):
+        values_sender={}
+        if data and data['use_sender_address']=="-1" :
+            values_sender['use_sender_address']=True
+            values_sender['sender_name'] = data['sender_name']
+            values_sender['sender_contact'] = data['sender_contact']
+            values_sender['sender_street'] = data['sender_street']
+            values_sender['sender_zip'] = data['sender_zip']
+            values_sender['sender_city'] = data['sender_city']
+            values_sender['sender_phone'] = data['sender_phone']
+            values_sender['sender_email'] = data['sender_email']
+            order = request.website.sale_get_order(force_create=1, context=None)
+            order.write(values_sender)
+        return super(website_sale_ext, self).checkout_values(data)
+
+
+
 
 
