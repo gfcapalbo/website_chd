f3719a2759f71e244c1ed035d15a21181bef6e02
gfcapalbo <giovanni@therp.nl>
2015-03-25 14:19:55 +0100

    [FIX] bug, wrote order before creation

--- website_chd_product_configurator/controllers/website_sale.py
+++ website_chd_product_configurator/controllers/website_sale.py
@@ -17,9 +17,6 @@ class website_sale_ext(websale.website_sale):
                 return pc.Chd_website.start(configurator,product.id)
         return super(website_sale_ext, self).product(product, category='', search='', **kwargs)
 
-
-
-
     def checkout_values(self, data=None):
         values_sender={}
         if data and data['use_sender_address']=="-1" :
@@ -31,9 +28,9 @@ class website_sale_ext(websale.website_sale):
             values_sender['sender_city'] = data['sender_city']
             values_sender['sender_phone'] = data['sender_phone']
             values_sender['sender_email'] = data['sender_email']
-            order = request.website.sale_get_order(force_create=1, context=None)
         elif data and data['use_sender_address']=="0":
             values_sender['use_sender_address']=False
+        order = request.website.sale_get_order(force_create=1, context=None)
         order.write(values_sender)
         return super(website_sale_ext, self).checkout_values(data)
 
